d9f44ea21be69d8895edaa00ba7f3f883e18fb47
Chinnell Davis <chinnell.davis@gmail.com>
2017-06-21 14:44:05 +0000

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // JavaScript File
 // JavaScript File
 $( document ).ready(function() {
+$("#hiking").hide();
+    $("#lang").click(function(){
 
+        $("#langChangeh1").html("Bienvenido al citio de Chinnell");
+        $("#langChangeh3").html("Aqui se encuentra oportunidades de escalar, acampar, y dar caminatas cerca de la ciudad de Nuevo York!")
+        $("#lang").html("Change the text to English");
+        $("#showPics").html("Por ejemplo...");
+        
+    });
+    
+    $("#showPic").click(function(){
+        $("#hiking").show();
+    });
 
 });
